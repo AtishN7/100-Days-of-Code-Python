f87d48c87f052f328eca15931e0751873704b52d
Atish Nayak <atishnayak25@gmail.com>
2026-08-23 13:15:54 -0400

    Turtle Crossing Project Final touches

--- Day-23-Turtle-Crossing/player.py
+++ Day-23-Turtle-Crossing/player.py
@@ -9,8 +9,20 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
-        self.color("DarkOliveGreen")
+        self.color("DarkGoldenRod")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def crossed_car_traffic(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+
+
 
--- Day-23-Turtle-Crossing/scoreboard.py
+++ Day-23-Turtle-Crossing/scoreboard.py
@@ -1,5 +1,28 @@
-FONT = ("Georgia", 24, "normal")
+from turtle import Turtle
+FONT = ("Georgia", 15, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 0
+        self.goto(-260, 280)
+        self.increase_level()
+
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.write(f"LEVEL: {self.level}", align="CENTER", font=FONT)
+
+    def game_over(self):
+        self.clear()
+        self.goto(0,0)
+        self.write(f"GAME OVER\n Final Level: {self.level}", align="CENTER", font=FONT)
+
+    def game_quit(self):
+        self.clear()
+        self.goto(0,0)
+        self.write(f"Final Level: {self.level}", align="CENTER", font=FONT)
