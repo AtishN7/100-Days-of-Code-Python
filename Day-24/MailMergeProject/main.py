b1910f064fb4edeb0b866cf7d0543a49c8933539
Atish Nayak <atishnayak25@gmail.com>
2026-08-27 07:15:01 -0400

    Day-24 Projects

---
+++ Day-24/MailMergeProject/main.py
@@ -0,0 +1,24 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+#
+# with open('./Input/Letters/starting_letter.txt', mode='r') as f:
+#     print(f.read())
+
+
+with open('./Input/Names/invited_names.txt', mode='r') as f:
+    names = f.readlines()
+
+for n in names:
+    with open('./Input/Letters/starting_letter.txt', mode='r') as letter:
+        contents = letter.read()
+        invitee = n.strip()
+        modified_letter = contents.replace("[name]", invitee)
+        letter_name = 'Letter_for_'+invitee+'.docx'
+        with open(f'./Output/ReadyToSend/{letter_name}', mode='w') as new_letter:
+            new_letter.write(modified_letter)
